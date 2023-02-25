49e4777110d2aba7a06afa11a2936c7b10d34aef
Alex Sarkisov <agsarkisov@gmail.com>
2023-02-25 23:58:28 +0100

    Add common navigation bar

---
+++ Airalo/Common/NavigationController.swift
@@ -0,0 +1,36 @@
+//
+//  NavigationController.swift
+//  Airalo
+//
+//  Created by Alex Sarkisov on 25.02.23.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialSetup()
+    }
+    
+    private final func initialSetup() {
+        view.backgroundColor = .background1
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.backgroundColor = .background1
+        navigationBarAppearance.largeTitleTextAttributes = [
+            .font: UIFont.semiBold(size: 27),
+            .foregroundColor: UIColor.content1,
+            .kern: -0.5,
+        ]
+        navigationBar.standardAppearance = navigationBarAppearance
+        navigationBar.scrollEdgeAppearance = navigationBarAppearance
+        navigationBar.barTintColor = .background1
+        navigationBar.tintColor = .content1
+        navigationBar.isTranslucent = false
+        navigationBar.prefersLargeTitles = true
+        navigationBar.layoutMargins.left = 20
+        navigationBar.layoutMargins.right = 20
+    }
+}
--- Airalo/Styles/Colors.swift
+++ Airalo/Styles/Colors.swift
@@ -10,6 +10,7 @@ import UIKit
 extension UIColor {
     
     static var content1: UIColor { UIColor(named: "Content/content1")! }
+    static var content2: UIColor { UIColor(named: "Content/content2")! }
     
     static var background1: UIColor { UIColor(named: "Background/background1")! }
 }
