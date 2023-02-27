8d690f9f8d184bd61357d4271fab22e2a52f2d60
Alex Sarkisov <agsarkisov@gmail.com>
2023-02-27 19:14:35 +0100

    Add area model

---
+++ Airalo/Models/Area.swift
@@ -0,0 +1,17 @@
+//
+//  Area.swift
+//  Airalo
+//
+//  Created by Alex Sarkisov on 27.02.23.
+//
+
+import Foundation
+
+struct Area: Decodable {
+    
+    let id: Int
+    let slug: String
+    let title: String
+    let image: ImageData
+    let packages: [Package]?
+}
