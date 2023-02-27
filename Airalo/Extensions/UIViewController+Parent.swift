01c8c752380971d8a7ab37e7056c779181b820ca
Alex Sarkisov <agsarkisov@gmail.com>
2023-02-27 19:18:25 +0100

    Fix ParentViewController extension

--- Airalo/Extensions/UIViewController+Parent.swift
+++ Airalo/Extensions/UIViewController+Parent.swift
@@ -18,8 +18,14 @@ extension ParentViewController {
     
     func addChild(_ childViewController: UIViewController, to containerView: UIView) {
         addChild(childViewController)
-        childViewController.view.frame = containerView.frame
         containerView.addSubview(childViewController.view)
+        childViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            childViewController.view.topAnchor.constraint(equalTo: containerView.topAnchor),
+            childViewController.view.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            childViewController.view.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            childViewController.view.trailingAnchor.constraint(equalTo: containerView.trailingAnchor)
+        ])
         childViewController.didMove(toParent: self)
     }
 
